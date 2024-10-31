0bb4f72c26fdda906ea7d86d5d38200564a4f503
KMusyimi <kennmusyimi97@gmail.com>
2024-10-31 20:44:48 +0300

    minor ui fixes v1.1

--- src/views/host/HostVanPricing.jsx
+++ src/views/host/HostVanPricing.jsx
@@ -4,6 +4,7 @@ export default function HostVanPricing() {
     const van = useOutletContext();
     return (<div>
         <p className='price-wrapper'>
-            <span className='price fw-700'>${van.price}</span><span className='period'>/day</span></p>
+            <span className='price fw-700'>${van.price}</span><span
+            className='period' style={{fontSize:'1.8rem'}}>/day</span></p>
     </div>)
 }
